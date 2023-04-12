af299337cc673b43f3c982fe5c9b89ec1c2e18f8
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-04-12 12:54:49 +0530

    c#

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -19,10 +19,29 @@ public class Program
         int y = 3;
         int result = x / y;
         Console.WriteLine(result);*/
-        int x = 1;
-        int y = 2;
+       // int x = 1;
+       // int y = 2;
         int z = 3;
 
-        Console.WriteLine(x + y * z);
+        //Console.WriteLine(x + y * z);
+
+       // int x = 5;
+      //  x = 8;
+       // Console.WriteLine(x + 1);
+
+     //   int x = 8;
+        int y = 3;
+
+      //  int result = x % y;
+       // Console.WriteLine(result);
+        int apples = 5;
+        apples++;
+        apples--;
+        apples--;
+        Console.WriteLine(apples);
+      //  int x = 10;
+      //  Console.WriteLine(x--);
+        int x = 10;
+        Console.WriteLine(--x);
     }
 }
